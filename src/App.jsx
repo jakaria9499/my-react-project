63172f66891fe4093d42a7d0fb39b6bc41f4b39a
jakaria <jakaria9499@gmail.com>
2025-09-19 11:54:30 +0600

    All added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Countries from './Countries';
+
+ function MyApp() {
+  return (
+    <>
+      <h1>Jakaria Mahmud</h1>
+      <Countries></Countries>
+    </>
+  );
+}
+
+
+export default MyApp;
